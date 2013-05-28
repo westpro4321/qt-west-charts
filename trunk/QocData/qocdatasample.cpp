11c31ec255025739ac0abe4a205aa4f0b348df69
L.R.Szewczyk90@gmail.com <L.R.Szewczyk90@gmail.com@5042d13b-4804-ced2-4704-347a9e3d6514>
2013-05-28 19:16:32 +0000

    Dodanie katalogu ze strukturami danych

---
+++ trunk/QocData/qocdatasample.cpp
@@ -0,0 +1,5 @@
+#include "qocdatasample.h"
+
+QocDataSample::QocDataSample()
+{
+}
---
+++ trunk/QocData/qocdatasample.h
@@ -0,0 +1,10 @@
+#ifndef QOCDATASAMPLE_H
+#define QOCDATASAMPLE_H
+
+class QocDataSample
+{
+public:
+	QocDataSample();
+};
+
+#endif // QOCDATASAMPLE_H
