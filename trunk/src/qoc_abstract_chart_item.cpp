8f9d1d538400b8a5ccce825f3bdcd537ee56f22f
L.R.Szewczyk90@gmail.com <L.R.Szewczyk90@gmail.com@5042d13b-4804-ced2-4704-347a9e3d6514>
2013-05-30 20:37:42 +0000

    Zalążki pierwszych interfejsów

---
+++ trunk/src/qoc_abstract_chart_item.cpp
@@ -0,0 +1,96 @@
+#include "qoc_abstract_chart_item.h"
+
+#include <QPen>
+#include <QBrush>
+
+QocAbstractChartItem::QocAbstractChartItem(const QString &title) :
+	m_titleAnchor(TopAnchor),
+	m_titleVisible(false),
+	m_visible(true),
+	m_pen(new QPen(QColor(Qt::black))),
+	m_brush(new QBrush(Qt::red))
+{
+	m_title = title;
+}
+
+QocAbstractChartItem::TitleAnchor QocAbstractChartItem::titleAnchor() const
+{
+	return m_titleAnchor;
+}
+
+void QocAbstractChartItem::setTitleAnchor(QocAbstractChartItem::TitleAnchor ta)
+{
+	m_titleAnchor = ta;
+}
+
+void QocAbstractChartItem::setTitle(QString t)
+{
+	m_title = t;
+}
+
+bool QocAbstractChartItem::isTitleVisible()
+{
+	return m_titleVisible;
+}
+
+void QocAbstractChartItem::setTitleVisible(bool b)
+{
+	m_titleVisible = b;
+}
+
+quint64 QocAbstractChartItem::z() const
+{
+	return m_z;
+}
+
+void QocAbstractChartItem::setZ(quint64 z)
+{
+	m_z = z;
+}
+
+QString QocAbstractChartItem::title() const
+{
+	return m_title;
+}
+
+QColor QocAbstractChartItem::color() const
+{
+	return m_color;
+}
+
+void QocAbstractChartItem::setColor(const QColor &c)
+{
+	m_color = c;
+}
+
+bool QocAbstractChartItem::isVisible()
+{
+	return m_visible;
+}
+
+void QocAbstractChartItem::setVisible(bool b)
+{
+	m_visible = b;
+}
+
+QPen QocAbstractChartItem::pen()
+{
+	return *m_pen;
+}
+
+void QocAbstractChartItem::setPen(QPen *pen)
+{
+	delete m_pen;
+	m_pen = pen;
+}
+
+QBrush QocAbstractChartItem::brush() const
+{
+	return *m_brush;
+}
+
+void QocAbstractChartItem::setBrush(QBrush *brush)
+{
+	delete m_brush;
+	m_brush = brush;
+}
---
+++ trunk/src/qoc_abstract_chart_item.h
@@ -0,0 +1,56 @@
+#ifndef QOC_ABSTRACT_CHART_ITEM_H
+#define QOC_ABSTRACT_CHART_ITEM_H
+
+#include <QRectF>
+#include <QString>
+#include <QColor>
+#include <QPen>
+#include <QBrush>
+
+class QocAbstractChartItem
+{
+public:
+	enum TitleAnchor{ TopAnchor, BottomAnchor, LeftAnchor, RightAnchor };
+
+	QocAbstractChartItem(const QString &title = QString());
+
+	virtual void draw(QPainter *painter, const QRectF &rect = QRectF()) = 0;
+
+	TitleAnchor titleAnchor() const;
+	void setTitleAnchor(TitleAnchor ta);
+	QString title() const;
+	void setTitle(QString t);
+	bool isTitleVisible();
+	void setTitleVisible(bool b);
+
+	virtual QRectF boundingRect() const = 0;
+
+	quint64 z() const;
+	void setZ(quint64 z);
+
+	QColor color() const;
+	void setColor(const QColor &c);
+
+	bool isVisible();
+	void setVisible(bool b);
+
+	QPen pen();
+	void setPen(QPen *pen);
+
+	QBrush brush() const;
+	void setBrush(QBrush *brush);
+
+
+protected:
+	TitleAnchor m_titleAnchor;
+	QString m_title;
+	bool m_titleVisible;
+	QRectF m_rect;
+	quint64 m_z;
+	QColor m_color;
+	bool m_visible;
+	QPen *m_pen;
+	QBrush *m_brush;
+};
+
+#endif // QOC_ABSTRACT_CHART_ITEM_H
