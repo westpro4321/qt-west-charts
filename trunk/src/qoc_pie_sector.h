aa7ac4ab27cfb0c74c57d43dd5daea3b3706f453
L.R.Szewczyk90@gmail.com <L.R.Szewczyk90@gmail.com@5042d13b-4804-ced2-4704-347a9e3d6514>
2013-06-09 13:58:14 +0000

    Wyświetlanie legendy

--- trunk/src/qoc_pie_sector.h
+++ trunk/src/qoc_pie_sector.h
@@ -5,7 +5,7 @@
 
 #include <QPointF>
 
-extern const int fullCircle;
+extern const int g_fullCircle;
 
 class QocPieChart;
 
@@ -13,17 +13,25 @@ class QocPieSector : public QocAbstractChartItem
 {
 	Q_OBJECT
 	Q_PROPERTY(uint offset READ offset WRITE setOffset)
+	Q_ENUMS(LabelMode)
 
 public:
+	enum LabelMode { TitleOnlyMode, PercentOnlyMode, TitlePercentMode };
+
 	QocPieSector(const QString &title = QString(), QObject *parent = 0);
 
 	virtual void draw(QPainter *painter, const QRectF &rect = QRectF());
+	virtual void drawTitle(QPainter *painter, const QRectF &rect = QRectF());
+
 
 	int startAngle() const;
 	int spanAngle() const;
 	virtual QRectF boundingRect() const;
 
 	uint offset();
+
+	LabelMode labelMode() const;
+	void setLabelMode(LabelMode mode);
 
 public slots:
 	void setStartAngle(int start);
@@ -32,13 +40,15 @@ public slots:
 
 private:
 	QRectF getRectWithOffset(const QRectF &rect);
-	QPointF getTitlePointF(const QRectF &rect);
+	QRectF getTitleRect(const QRectF &rect);
+	double percentage();
 
 private:
 	QocPieChart *m_chart;
 	int m_startAngle;
 	int m_spanAngle;
 	uint m_offset;
+	LabelMode m_mode;
 };
 
 #endif // QOC_PIE_SECTOR_H
