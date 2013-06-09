f9b1f9d40a289a539dd7fc66defa431b6bafd41b
L.R.Szewczyk90 <L.R.Szewczyk90@gmail.com>
2013-06-09 19:06:21 +0000

    Powrót do wspólnej klasy bazowej wykresów.

--- src/qoc_pie_chart.h
+++ src/qoc_pie_chart.h
@@ -1,6 +1,7 @@
 #ifndef QOC_PIE_CHART_H
 #define QOC_PIE_CHART_H
 
+#include "qoc_abstract_chart.h"
 #include "qoc_data_series_owner.h"
 #include "qoc_pie_sector.h"
 
@@ -9,7 +10,7 @@
 #include <QFont>
 
 
-class QocPieChart : public QObject, QocDataSeriesOwner<QPointF>
+class QocPieChart : public QocAbstractChart, QocDataSeriesOwner<QPointF>
 {
 	Q_OBJECT
 	Q_PROPERTY(int startAngle READ startAngle WRITE setStartAngle)
@@ -18,43 +19,19 @@ public:
 	explicit QocPieChart(QObject *parent = 0);
 	
 	virtual void draw(QPainter *painter, const QRectF &rect = QRectF());
+	virtual QList<QocPieSector *> items();
 	virtual void setSeries(QocDataSeries<QPointF> *ds);
 
-	bool isAntialiased();
-	void setAntialiased(bool b);
-
-	QBrush background() const;
-	void setBackground(QBrush *brush);
-
-	double topMargin();
-	void setTopMargin(double fraction);
-	double bottomMargin();
-	void setBottomMargin(double fraction);
-	double leftMargin();
-	void setLeftMargin(double fraction);
-	double rightMargin();
-	void setRightMargin(double fraction);
-
-	void setMargins(double top, double bottom, double left, double right);
 
 	QocPieSector *item(double x);
+	QocPieSector *itemAt(size_t i);
+
 	virtual void repaint();
 
 	int startAngle() const;
 	QRectF printRect() const;
 	QRectF itemsRect() const;
 
-
-	QString title() const;
-	void setTitle(const QString &title);
-	QFont titleFont() const;
-	void setTitleFont(const QFont &font);
-	bool isTitleVisible() const;
-	void setTitleVisible(bool b);
-	int titleFlags() const;
-	void setTitleFlags(int flags);
-
-	QList<QocPieSector *> items();
 
 public slots:
 	void setStartAngle(int start);
@@ -69,21 +46,9 @@ protected:
 
 protected:
 	QList<QocPieSector *> m_items;
-	bool m_antialiased;
-	QBrush *m_backgroundBrush;
-	double m_topMargin;
-	double m_bottomMargin;
-	double m_leftMargin;
-	double m_rightMargin;
 	QRectF m_printRect;
 	QRectF m_itemsRect;
-
 	int m_startAngle;
-
-	QString m_title;
-	QFont m_titleFont;
-	bool m_titleVisible;
-	int m_titleFlags;
 
 private:
 	QRectF getItemsRect(const QRectF &r);
