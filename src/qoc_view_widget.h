b9c7e0c4ccb6e6e2d114432194d28f559e4497d5
L.R.Szewczyk90 <L.R.Szewczyk90@gmail.com>
2013-09-19 23:29:30 +0000

    KONIEC

--- src/qoc_view_widget.h
+++ src/qoc_view_widget.h
@@ -17,15 +17,18 @@ public:
 	bool event(QEvent *);
 	void setChart(QocAbstractChart *c);
 
+	virtual bool eventFilter(QObject *watched, QEvent *event);
 signals:
 	void animationEnded(bool);
 public slots:
-	void rebuildChart() const;
+	void rebuildChart();
 	void animationFinished();
 protected:
 	virtual void paintEvent(QPaintEvent * event);
 private:
 	QocAbstractChart *m_chart;
+	bool m_animationStarted;
+	int m_events;
 };
 
 #endif // QOC_VIEW_WIDGET_H
